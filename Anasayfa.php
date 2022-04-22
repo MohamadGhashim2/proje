235da4146069d66f10396f4523d9a37446782194
MohamadGhashim <mohamad.ghashim@ogr.sakarya.edu.tr>
2022-04-22 22:37:41 +0300

    first commit

---
+++ Anasayfa.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="icon" type="text/css" href="IMG/favicon.ico">
+
+    <title> Ana sayfa</title>
+</head>
+<body>
+
+
+<h1>   
+	Welcome <?php echo $_POST['Username'];?>
+
+	</h1>
+</body>
+</html>
