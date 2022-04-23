f375f9aa2e5fb5a2d3f2a8e7cdda08f8685a63dc
MohamadGhashim <mohamad.ghashim@ogr.sakarya.edu.tr>
2022-04-23 16:24:45 +0300

    first change

--- Anasayfa.php
+++ Anasayfa.php
@@ -7,11 +7,23 @@
     <title> Ana sayfa</title>
 </head>
 <body>
+<header>
+    
+</header>
+<aside>
+ <a href="odev.html"><button type="button" class="Button-log" >Log In</button></a>
+ <a href="odev.html"><button type="button" class="Button-log">Sign up</button></a>
+ <div class="welcome">
+      <h3>
+     Welcome <br> <?php echo $_POST['Username'];?>
+      </h3>     
+ </div>
 
 
-<h1>   
-	Welcome <?php echo $_POST['Username'];?>
 
-	</h1>
+
+</aside>
+
+
 </body>
 </html>
