7a3cfb0719303923c6b3ad3db9de4ea798d7f561
MohamadGhashim <mohamad.ghashim@ogr.sakarya.edu.tr>
2022-05-10 23:01:15 +0300

    nav

--- Anasayfa.php
+++ Anasayfa.php
@@ -3,6 +3,10 @@
 <html>
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="keywords" content="HTML, CSS, JavaScript">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="icon" type="text/css" href="IMG/favicon.ico">
@@ -21,32 +25,40 @@
                 </div>
             </div>
         </div>
-        <div class="row text-light bg-color ">
-            <div class="col text-end">
-                <div class="badge bg-secondary">
-                    <h1>
-Anasayfa</h1>
-                </div>
-            </div>
-            <div class="col">
-                2
-            </div>
-            <div class="col">
-                3
-            </div>
-            <div class="col">
-                4
-            </div>
-            <div class="col">
-                5
-            </div>
-            <div class="col">
-                6
-            </div>
-
-
-        </div>
     </div>
+    <nav class="navbar navbar-expand navbar-dark bg-color">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="Giris.html">Login</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-mdb-toggle="collapse"
+      data-mdb-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i class="fas fa-bars"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="Anasayfa.php">Anasayfa</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Sehirm.php">Şehrim</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="hobilerim.php">Hobilerim</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="ileitism.php">İletişim</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
 
 </body>
 
